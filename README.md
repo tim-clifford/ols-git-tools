56d9099a389f0674a31dcdbd43a79b504bc2d133
Tim Clifford <tclifford@protonmail.com>
2021-03-20 12:43:56 +0000

    Make installer robust; use GPLv3; update README.md
    Now using setup script based on sldworks-git-tools

--- README.md
+++ README.md
@@ -2,12 +2,35 @@
 
 Provides integration with git for syncing overleaf projects with
 [overleaf-sync](https://github.com/moritzgloeckl/overleaf-sync).
-Add this repo as a submodule, setup overleaf-sync as described in their README,
-and then run `./ols-git-tools/setup.sh` from the project directory to alias
-the syncing tool to `git sync` for the current project.
 
 Don't get any ideas, I don't like Overleaf at all. This just makes it easier to
 collaborate with wysiwig people.
 
 There are a lot of possible edge cases so it's possible I've missed one of
 them. Please open an issue if anything isn't working as expected.
+
+To set up the integration, first install
+[overleaf-sync](https://github.com/moritzgloeckl/overleaf-sync) and log in with
+```
+ols login [-u/--username -p/--pasword --path]
+```
+note that ols requires that the name of your folder is the same as the name of
+the project on overleaf.
+
+Then add this repo as a submodule and setup the git tools by running the
+following from any Linux/MacOS terminal, or Git Bash on Windows
+```
+git submodule update --init
+./ols-git-tools/setup.sh
+```
+
+You can now use `git sync` to merge the overleaf changes into git. Note that
+the automatic merge may not achieve your goals (it forces a conflict) but you
+can manually compare the changes with `git diff HEAD~`
+
+Also note (from the
+[overleaf-sync](https://github.com/moritzgloeckl/overleaf-sync) docs):
+
+> When modifying a file on Overleaf and immediately syncing afterwards, the
+> tool might not detect the changes. Please allow 1-2 minutes after modifying a
+> file on Overleaf before syncing it to your local computer.
--- setup.sh
+++ setup.sh
@@ -1,2 +1,62 @@
 #!/bin/sh
-cat ols-git-tools/gitconfig >> .git/config
+#
+# Overleaf Git Tools
+# This file is based on the equivalent in SolidWorks Git Tools
+#
+# Copyright (C) 2021 Tim Clifford
+# Copyright (C) 2021 Tim Clifford, Henry Franks
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+# Remove existing sld config if it exists and replace it with the latest config
+header_start="\
+### ---------------- BEGIN AUTO ADDED OVERLEAF TOOLS ---------------------- ###\
+"
+header_end="\
+### ----------------- END AUTO ADDED OVERLEAF TOOLS ----------------------- ###\
+"
+
+setup_config() { # args (header_start, header_end, file to add, file to add to)
+
+	if ! [ -f $4 ]; then
+		# Thanks, I hate it
+		echo "\
+$1
+$(cat "$3")
+$2" >> $4
+
+	elif cat $4 | grep -q "$1"; then
+		echo "Found existing overleaf config in $4, replacing..."
+		# This is cursed, but at least it's not vba...
+		f="\
+$(grep -B9999 "$1" $4 | grep -v "$1")
+$1
+$(cat "$3")
+$2
+$(grep -A9999 "$2" $4 | grep -v "$2")"
+		# Can't oneline this because of order of operations
+		echo "$f" > $4
+	else
+		# Thanks, I hate it
+		echo "\
+$1
+$(cat "$3")
+$2" >> $4
+	fi
+}
+
+setup_config \
+	"$header_start" "$header_end" \
+	"ols-git-tools/gitconfig" \
+	".git/config"
